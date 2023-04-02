0d41c2a6687569540e61e54c33fb0270f1bccc60
valleymouth1216 <okeitan3280@gmail.com>
2023-04-02 18:28:14 +0900

    20230402

---
+++ JavaWcp/src/Ready03.java
@@ -0,0 +1,58 @@
+
+public class Ready03 {
+	 public static void main(String[] args) {
+		 System.out.println("// 1行のコメント例");
+		 System.out.println("");
+		 System.out.println("/*");
+		 System.out.println("複数行の");
+		 System.out.println("コメント例");
+		 System.out.println("*/");
+		 System.out.println("--------------");
+		 System.out.println("↓ クラスブロックの始まり");
+		 System.out.println("public class Ready02 {");
+		 System.out.println("↓ メソッドブロックの始まり (インデントしてわかりやすくする)");
+		 System.out.println(" public static void main(String[] args) {");
+		 System.out.println("↓ コメントを追加 (メソッドブロックよりさらにインデントしてわかりやすくする)");
+		 System.out.println("  // メソッドの処理を記述");
+		 System.out.println("System.out.println(\"Hello World!\");   ← 文字列を出力する処理");
+		 System.out.println("} ");
+		 System.out.println(" ↑ メソッドブロックの終わり");
+		 System.out.println("}");
+		 System.out.println("↑ クラスブロックの終わり");
+		 System.out.println("--------------");
+		 System.out.println("Hello Java!");
+		 System.out.println("Hello WEBCAMP");
+		 System.out.println("Hello Java!");
+		 System.out.println("Hello WEBCAMP");
+		 System.out.println("Hello Java!");
+		 
+	 }
+	 
+	// 1行のコメント例
+
+	/*
+	複数行の
+	コメント例
+	*/
+
+}
+
+//↓ クラスブロックの始まり
+//public class Ready02 {
+//    ↓ メソッドブロックの始まり (インデントしてわかりやすくする)
+//    public static void main(String[] args) {
+//        ↓ コメントを追加 (メソッドブロックよりさらにインデントしてわかりやすくする)
+//        // メソッドの処理を記述
+//        System.out.println("Hello World!");   ← 文字列を出力する処理
+//    } 
+//    ↑ メソッドブロックの終わり
+//}
+//↑ クラスブロックの終わり
+
+//mainメゾット 2種類
+//public static void main(String[] args) {
+// 処理（文）
+//}
+//public static void main(String... args) {
+// 処理（文）
+//}
