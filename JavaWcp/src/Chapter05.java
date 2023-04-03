25821b8c3861728d8cb50a75cd1560042019fcbc
valleymouth1216 <okeitan3280@gmail.com>
2023-04-04 00:05:00 +0900

    javaカリキュラム5章

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,59 @@
+
+public class Chapter05 {
+ public static void main(String[] args) {
+	 int int1;
+	 int1 =1234567890;
+	 System.out.println(int1);
+	 
+	 long long1;
+	 long1 =1234567890;
+	 System.out.println(long1);
+	 
+	 double double1;
+	 double1 =4.5;
+	 System.out.println(double1);
+	 
+	 boolean boolean1;
+	 boolean1 =true;
+	 System.out.println(boolean1);
+	 
+	 float float1;
+	 float1 =12;
+	 System.out.println(float1);
+
+	 Integer Integer1;
+	 Integer1 =12345;
+	 System.out.println(Integer1);
+
+	 Character Character1;
+	 Character1 =12345;
+	 System.out.println(Character1);
+	 
+	 
+	 int[]array1 = new int[3];
+	 array1[0] =0;
+	 array1[1] =1;
+	 array1[2] =3;
+	 array1[3] =5;
+	 System.out.println(array1[3]);
+	 
+	 
+     long l = 10000000000L;
+     System.out.println(l);
+     double d = 3.14;
+     System.out.println(d);
+     boolean b = true;
+     System.out.println(b);
+
+     Integer i = 100;
+     System.out.println(i);
+     Float f = 1.4142f;
+     System.out.println(f);
+     Character c = 'う';
+     System.out.println(c);
+
+     int[] array = { 0, 10, 20, 30, 40 };
+     System.out.println(array[3]);
+			 
+	 }
+}
