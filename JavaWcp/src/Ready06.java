e21e811c6b86becf324b52e49b4b8eee85659422
valleymouth1216 <okeitan3280@gmail.com>
2023-04-05 22:57:26 +0900

    20230405-java6章

---
+++ JavaWcp/src/Ready06.java
@@ -0,0 +1,30 @@
+
+public class Ready06 {
+ public static void main(String args[]) {
+	 int a = 15;
+	 int c;
+	 c = a + 3;  // c = 18
+	 System.out.println(c = a + 3);
+	 
+	 c = a - 5;  // c = 10
+	 System.out.println(c = a -5 );
+	 
+	 c = a * 2;  // c = 30
+	 System.out.println(c = a * 2);
+	 
+	 c = a / 6;  // c = 2
+	 System.out.println(c = a / 6);
+	 
+	 c = a % 4;  // c = 3
+	 System.out.println(c = a % 4);
+	 
+	 
+	 
+	 
+	 int b;
+	 b = 10;
+	 b++;  // a = 11
+	 b = 10;
+	 b--;  // a = 9
+ }
+}
