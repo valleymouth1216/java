8965c20f5c7c65d6568311adf11721d50dc89574
valleymouth1216 <okeitan3280@gmail.com>
2023-04-06 23:42:50 +0900

    20230406-java6章

--- JavaWcp/src/Ready06.java
+++ JavaWcp/src/Ready06.java
@@ -50,5 +50,87 @@ public class Ready06 {
 	 System.out.println(cd);
 	 // それぞれの値は、a が「6」で c が「5」
 	 
+	 //代入演算子
+	 
+	 int e;
+
+	 e = 10;  // a = 10：aに10を代入
+	 System.out.println(e);
+	 
+	 e += 5;  // a = 15：aにa+5の結果を代入
+	 System.out.println(e);
+	 
+	 e -= 3;  // a = 12：aにa-3の結果を代入
+	 System.out.println(e);
+	 
+	 e *= 4;  // a = 48：aにa*4の結果を代入
+	 System.out.println(e);
+	 
+	 e /= 2;  // a = 24：aにa/2の結果を代入
+	 System.out.println(e);
+	 
+	 e %= 7;  // a = 3 ：aにa%7の結果を代入
+	 System.out.print(e);
+	 
+	 //関係演算子
+	 int aa = 5;
+	 boolean ab;
+
+	 ab = aa < 5;   // ab = false
+	 System.out.println(ab);
+	 ab = aa <= 5;  // ab = true
+	 System.out.println(ab);
+	 ab = aa > 4;   // ab = true
+	 System.out.println(ab);
+	 ab = aa>= 6;  // ab = false
+	 System.out.println(ab);
+	 ab = aa == 5;  // ab = true
+	 System.out.println(ab);
+	 ab = aa!= 5;  // ab = false
+	 System.out.println(ab);
+	 
+	 //論理演算子
+	 boolean ac = true;
+	 boolean ae = false;
+	 boolean ad;
+
+	 ae = ac && true;   // c = true
+	 System.out.println(ae);
+	 ae = ac && false;  // c = false
+	 System.out.println(ae);
+	 ae = ab || true;   // c = true
+	 System.out.println(ae);
+	 ae = ab || false;  // c = false
+	 System.out.println(ae);
+	 ae = !ac;          // c = false
+	 System.out.println(ae);
+	 ae = !ab;          // c = true
+	 System.out.println(ae);
+	 
+	 
+	 //文字列の連結
+	 String ba = "100";
+	 String bb = "200";
+	 String bc;
+	 bc = ba + bb;        // c = 100200
+	 System.out.println(bc);
+	 bc = "あ" + "い";   // c = あい
+	 System.out.println(bc);
+	 
+	 
+	 //文字列の等価比較
+	 String da = "applePie";
+	 String dd = "apple";
+	 String de = dd + "Pie";
+	 dc = da == de;  // c = false
+	 
+	 String ea = "applePie";
+	 String ef = new String("applePie");
+	 ec = ea == ef;  // c = false
+	 
+	// 変数の中身はすぐ上の例と同じ
+	 c = a.equals(b);  // c = true
+	 c = a.equals(e);  // c = true
+	 c = a.equals(f);  // c = true
  }
 }
