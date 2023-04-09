499ca49b58e6c82c711ef33fa14237f159489988
valleymouth1216 <okeitan3280@gmail.com>
2023-04-09 20:47:58 +0900

    20230409-java7章

--- JavaWcp/src/Ready07.java
+++ JavaWcp/src/Ready07.java
@@ -17,6 +17,8 @@ public class Ready07 {
      //ブロック(処理文);
      //}  
      
+     
+     System.out.println( "---------");
      int number1 = 7;
      // if文：
      // numberが5未満の場合、ブロックの処理を実行
@@ -58,6 +60,33 @@ public class Ready07 {
      //	} else {
      //	    ブロック(処理文)
      //	}
+     System.out.println( "---------");
+
+         int number3 = 7;
+         // if文：
+         // numberが5未満の場合、ブロックの処理を実行
+         if (number3 < 5) {
+             // ブロック：「○ は5未満です」を出力
+             System.out.println(number3 + " は5未満です");
+         // else-if文：
+         // 前のif文、-の条件式の評価が全てflaseで、
+         // numberが10未満の場合、ブロックの処理を実行
+         } else if (number3 < 10) {
+             // 評価がtrueなので、処理が実行される
+             // ブロック：「○ は5以上、10未満です」を出力
+             System.out.println(number3 + " は5以上、10未満です");
+         // else-if文：
+         // 前の条件式の評価が全てflaseで、numberが15未満の場合、ブロックの処理を実行
+         } else if (number3 < 15) {
+             // 評価はtrueだが、前の評価がtrueのため実行されない
+             // ブロック：「○ は10以上、15未満です」を出力
+             System.out.println(number3 + " は10以上、15未満です");
+         // else文：
+         // 前の条件式の評価が全てflaseの場合、ブロックの処理を実行
+         } else {
+             System.out.println(number3 + " は15未満でないです");
+
+     }
  }
 	
 }
