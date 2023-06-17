41c749bdb1837e879c922e00a1c6d6ec79426002
valleymouth1216 <okeitan3280@gmail.com>
2023-06-17 23:51:19 +0900

    20230617

--- JavaWcp/src/classMethod/human/ClassMethod03.java
+++ JavaWcp/src/classMethod/human/ClassMethod03.java
@@ -4,6 +4,8 @@ public class ClassMethod03 {
     public static void main(String[] args) {
         Ready09_3 yamada = new Ready09_3();
 
+        
+        
         System.out.println("私の名前は、" + yamada.name + "です。");
         String profile = yamada.getProfile();
         System.out.println(profile + "です。");
