b25ce2052ecf808b108eabc5d06a1faaa6bfffc4
valleymouth1216 <okeitan3280@gmail.com>
2023-07-08 23:50:06 +0900

    20230708

--- JavaWcp/src/o.java
+++ JavaWcp/src/o.java
@@ -1,4 +1,32 @@
+import classMethod.human.Ready09_3;
 
 public class o {
 
+    public static void main(String[] args) {
+        Ready09_3 yamada = new Ready09_3();
+
+        
+        
+        
+        
+        
+        
+        
+        System.out.println("私の名前は、" + yamada.name + "です。");
+        String profile = yamada.getProfile();
+        System.out.println(profile + "です。");
+
+        
+        
+        
+        
+        
+        
+        
+        
+        yamada.greet("田辺");
+        yamada.greet(null);
+    }
+	
+	
 }
